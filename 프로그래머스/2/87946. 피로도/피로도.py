751643372b2be71f82586d52e8651558c9d04f71
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2024-11-08 15:58:15 +0900

    [level 2] Title: 피로도, Time: 52.85 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,16 @@
 def solution(k, dungeons):
-    answer = [0] 
-    visited = [False for _ in range(len(dungeons))]
-    dfs(k, 0, dungeons, visited, answer)
+    answer = [-1]
+    
+    visited = [False] * len(dungeons)
+    dfs(k, dungeons, visited, answer, 0)
+    
     return answer[0]
 
-def dfs(k, cnt, dungeons, visited, answer):
-    if cnt > answer[0]:
-        answer[0] = cnt
+def dfs(k, dungeons, visited, answer, score ):
     
+    answer[0] = max(answer[0], score)
     for i in range(len(dungeons)):
-        if not visited[i] and k >= dungeons[i][0]:
+        if not visited[i] and k>= dungeons[i][0]:
             visited[i] = True
-            dfs(k - dungeons[i][1], cnt + 1, dungeons, visited, answer)
+            dfs(k-dungeons[i][1], dungeons, visited, answer, score+1)
             visited[i] = False
