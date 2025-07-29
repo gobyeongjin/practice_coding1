01ffe79c3a57b8aad7eea680c2059fd61b1b2cde
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2025-07-29 16:21:41 +0900

    [Silver I] Title: 정수 삼각형, Time: 84 ms, Memory: 36504 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+
+lst = [list(map(int,input().split())) for _ in range(n)]
+
+
+def solution(triangle):
+
+    floor = len(triangle) - 1
+
+    while floor > 0:
+
+        for i in range(floor):
+            triangle[floor-1][i] += max(triangle[floor][i], triangle[floor][i+1])
+        
+        floor -= 1
+    
+    return triangle[0][0]
+
+print(solution(lst))
