139f24bcd3e42a1ca7f9232c61f0c1344379d7bc
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2025-08-08 16:06:21 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 404 ms, Memory: 41348 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,36 +1,40 @@
 from collections import deque
 
-def bfs(i, j):
-    q = deque([(i, j)])
-    set1 = set()
-    
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
-    
-    answer = 0
-    
-    while q:
-        a, b = q.popleft()
-        for i in range(4):
-            nx, ny = a + dx[i], b + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and (nx, ny) not in set1:
-                if lst[nx][ny] == "O":
-                    q.append((nx, ny))
-                    set1.add((nx, ny))
-                elif lst[nx][ny] == "P":
-                    answer += 1
-                    q.append((nx, ny))
-                    set1.add((nx, ny))
-                    
-    return answer
+N , M = map(int,input().split())
+school = [ list(input()) for i in range(N)]
 
-n, m = map(int, input().split())
-lst = [list(input().rstrip()) for _ in range(n)]
 
-for i in range(n):
-    for j in range(m):
-        if lst[i][j] == 'I':
-            if bfs(i,j)==0:
-                print('TT')
-            else:
-                print(bfs(i,j))
+visited = [M*[False] for _ in range(N)]
+
+def bfs (x,y):
+    visited[x][y] = True
+    count = 0
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    que = deque([(x,y)])
+
+    while que:
+        i,j = que.popleft()
+        for k in range(4):
+            nx = i+dx[k]
+            ny = j+dy[k]
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if not visited[nx][ny] and school[nx][ny] != 'X':
+                    visited[nx][ny] = True
+                    que.append((nx, ny))
+                    if school[nx][ny] == 'P':
+                        count += 1
+    
+    return count
+
+
+
+for i in range(N):
+    for j in range(M):
+        if school[i][j] == 'I':
+            result = bfs(i,j)
+
+print(result if result > 0 else 'TT')
+
+
