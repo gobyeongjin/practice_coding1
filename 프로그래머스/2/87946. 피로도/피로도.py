a170c4deecbb9995af41745cc514ffbc3956657f
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-08-15 16:16:16 +0900

    [level 2] Title: 피로도, Time: 18.30 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,16 +1,24 @@
-def solution(k, dungeons):
-    answer = [-1]
-    
-    visited = [False] * len(dungeons)
-    dfs(k, dungeons, visited, answer, 0)
-    
-    return answer[0]
+from itertools import permutations
 
-def dfs(k, dungeons, visited, answer, score ):
-    
-    answer[0] = max(answer[0], score)
-    for i in range(len(dungeons)):
-        if not visited[i] and k>= dungeons[i][0]:
-            visited[i] = True
-            dfs(k-dungeons[i][1], dungeons, visited, answer, score+1)
-            visited[i] = False
+def solution(k, dungeons):
+    answer = 0
+
+    # 던전의 인덱스
+    indexes = range(len(dungeons))
+
+    # 모든 던전 방문 순서 생성
+    for order in permutations(indexes):
+        current_k = k
+        count = 0
+
+        for i in order:
+            # 현재 피로도로 던전을 갈 수 있는지 확인
+            if current_k >= dungeons[i][0]:
+                current_k -= dungeons[i][1]
+                count += 1
+            else:
+                break
+
+        answer = max(answer, count)
+
+    return answer
