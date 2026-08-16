af1ba759e9e0775bb6be421da495536bca6ea365
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-08-16 12:42:03 +0900

    [level 2] Title: 피보나치 수, Time: 389.61 ms, Memory: 457 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    
+    f = (n+1)*[0]
+    
+    f[0] = 0
+    f[1] = 1
+    
+    
+    
+    for i in range(2,n+1):
+        f[i] = f[i-1]+f[i-2]
+    
+    return f[n]  %1234567
