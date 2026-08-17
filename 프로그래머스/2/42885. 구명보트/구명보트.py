4340521509949ce46654e550cb3a58ff7c9b88c4
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-08-17 14:33:39 +0900

    [level 2] Title: 구명보트, Time: 7.36 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,14 +1,19 @@
 def solution(people, limit):
     answer = 0
+    
     people.sort()
     
-    st = 0
-    end = len(people) - 1
+    left = 0
+    right =len(people)-1
     
-    while st <= end:
-        if people[st] + people[end] <= limit:
-            st += 1   # 가벼운 사람도 태움
-        end -= 1       # 무거운 사람은 항상 태움
-        answer += 1    # 보트 1대 사용
-    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            right -= 1
+            answer +=1
+        
+        else:
+            right -= 1
+            answer +=1 
+        
     return answer
