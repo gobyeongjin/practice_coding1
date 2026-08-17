dffed497170b42f8f3b1a7d65aa1d99358f9b6c3
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-08-17 14:15:21 +0900

    [level 2] Title: 할인 행사, Time: 94.29 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,14 +2,11 @@ from collections import Counter
 
 def solution(want, number, discount):
     answer = 0
+    want_zip = dict(zip(want,number))
     
-    lst = []
-    want_counter = dict(zip(want,number))
-    
-    for i in range(len(discount)-9):
-        if want_counter == Counter(discount[i:i+10]):
+    for i in range(0,len(discount)-9):
+        if Counter(discount[i:i+10]) == want_zip:
             answer+=1
-        else:
-            continue
-    
+        
+        
     return answer
