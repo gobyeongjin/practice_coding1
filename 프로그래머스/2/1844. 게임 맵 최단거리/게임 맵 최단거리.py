644d912f5deae4f90c75bbd8a9616a90fde3f73b
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-08-21 12:45:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.92 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,37 +1,34 @@
 from collections import deque
 
 def solution(maps):
+    return bfs(maps, 0, 0)
+
+def bfs(maps, x, y):
+
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
-    def bfs(x, y):
-        queue = deque()
-        queue.append((x, y))
-        
-        while queue:
-            x, y = queue.popleft()
-            
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                
-            
-                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
-                    continue
-                
-                
-                if maps[nx][ny] == 0:
-                    continue
-          
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    queue.append((nx, ny))
-        
+    queue = deque()
+    queue.append((x, y))
 
-        return maps[len(maps)-1][len(maps[0])-1]
+    while queue:
+        x, y = queue.popleft()
 
-    maps[0][0] = 1
-    answer = bfs(0, 0)
-    return -1 if answer == 1 else answer
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+            if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
+                continue
 
+            if maps[nx][ny] == 0:
+                continue
+
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                queue.append((nx, ny))
+
+    if maps[-1][-1] == 1:
+        return -1
+
+    return maps[-1][-1]
