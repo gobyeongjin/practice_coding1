358f61f8355a26de5d7d1b72d1ecb960690964b6
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-08-25 19:24:35 +0900

    [level 1] Title: 햄버거 만들기, Time: 129.44 ms, Memory: 28.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,14 @@
+def solution(ingredient):
+    answer = 0
+    lst = []
+    
+    for i in ingredient:
+        lst.append(i)
+        if lst[-4:] == [1,2,3,1]:
+            answer += 1 
+            for i in range(4):
+                lst.pop()
+
+    
+        
+    return answer
