7f6feb9e6f63e5da263969d6a636917fd52fc0c6
gobyeongjin <95464202+gobyeongjin@users.noreply.github.com>
2026-09-10 12:51:32 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,10 +1,17 @@
 def solution(n):
     answer = 0
-    dp = [0]*(n+1)
-    dp[0] = 1
-    dp[1] = 1
     
-    for i in range(2,n+1):
-        dp[i] = dp[i-2] + dp[i-1]
+    dp = [0] *(n+1)
+    
+    if n<3:
+        return n
+    
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+    
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
         
-    return dp[n] %  1234567
+    return dp[i] % 1234567
